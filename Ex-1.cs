c76a564c16345824aef1e18046effaebe941de2d
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-06-28 19:50:02 -0300

    Add project files.

---
+++ Ex-1.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio1;
+
+class Exercicio1 {
+    static void Main()
+    {
+        Console.Write("Digite seu primeiro nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine($"Olá, {nome}! Seja muito bem-vindo!");
+    }
+}
+
